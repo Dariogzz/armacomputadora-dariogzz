ab89dc402b14bd29121d029062abff3db420dc7a
Dgzz <al45764@un.edu.mx>
2025-11-28 09:17:45 -0600

    comit 6 avance de mi script

---
+++ calculos.js
@@ -0,0 +1,12 @@
+var formulario = document.getElementById('formulariocomputadora');
+function calcular() {
+var total = 0;
+var gabinete = document.getElementById("Gabinete").value;
+
+var procesador = document.getElementById("Procesador").value;
+
+var discoduro = document.getElementById("Discoduro").value;
+
+var memoriaram = document.getElementById("MemoriaRAM").value;
+
+var monitor = document.getElementById("Monitor").value;
