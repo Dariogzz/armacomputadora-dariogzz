ef8502c505014ec450c18be0e83ffba285d8908a
Dgzz <al45764@un.edu.mx>
2025-11-28 09:46:40 -0600

    8 comit

--- calculos.js
+++ calculos.js
@@ -10,3 +10,61 @@ var discoduro = document.getElementById("Discoduro").value;
 var memoriaram = document.getElementById("MemoriaRAM").value;
 
 var monitor = document.getElementById("Monitor").value;
+
+if (gabinete == "Minitorre") {
+        total = total + 700;
+    } else if (gabinete == "Torre") {
+        total = total + 1200;
+    } else if (gabinete == "Desktop") {
+        total = total + 550;
+    } else if (gabinete == "Gamer") {
+        total = total + 2500;
+    }
+
+ if (procesador == "Celeron") {
+        total = total + 900;
+    } else if (procesador == "Intel i3") {
+        total = total + 1500;
+    } else if (procesador == "Intel i5") {
+        total = total + 3500;
+    } else if (procesador == "Intel i7") {
+        total = total + 5000;
+    }
+
+    if (discoduro == "500GB") {
+        total = total + 500;
+    } else if (discoduro == "750GB") {
+        total = total + 800;
+    } else if (discoduro == "1TB") {
+        total = total + 1000;
+    } else if (discoduro == "250GB SSD") {
+        total = total + 900;
+    } else if (discoduro == "500GB SSD") {
+        total = total + 1380;
+    }
+
+    if (memoriaram == "4 GB") {
+        total = total + 600;
+    } else if (memoriaram == "8 GB") {
+        total = total + 1050;
+    } else if (memoriaram == "16 GB") {
+        total = total + 1950;
+    }
+
+    if (monitor == '17"') {
+        total = total + 1500;
+    } else if (monitor == '19"') {
+        total = total + 1800;
+    } else if (monitor == '21.5"') {
+        total = total + 2200;
+    } else if (monitor == '24"') {
+        total = total + 2600;
+    } else if (monitor == '27"') {
+        total = total + 3000;
+    } else if (monitor == '32"') {
+        total = total + 3500;
+    }
+
+
+    alert("El total es: $" + total);
+}
